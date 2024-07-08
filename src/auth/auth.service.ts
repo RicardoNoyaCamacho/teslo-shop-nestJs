0d23f8de3a7bfccd009f5b6800e02530f3bfc19e
RicardoNoyaCamacho <83376211+RicardoNoyaCamacho@users.noreply.github.com>
2024-07-08 16:48:47 -0600

    refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -62,6 +62,13 @@ export class AuthService {
 
   }
 
+  checkAuthStatus(user: User) {
+    return {
+      ...user,
+      toke: this.getJwtToken({ id: user.id })
+    };
+  }
+
   private getJwtToken(payload: JwtPayload) {
 
     const token = this.jwtService.sign(payload);
